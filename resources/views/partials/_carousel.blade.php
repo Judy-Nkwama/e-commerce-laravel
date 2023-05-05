63c73cdb383cade40900ea12bc0b92c93b13cea8
Judy Nkwama <nkwamajudy@gmail.com>
2023-05-05 04:01:54 +0200

    Working on the footer

--- resources/views/partials/_carousel.blade.php
+++ resources/views/partials/_carousel.blade.php
@@ -1,9 +1,8 @@
-<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+<div id="myCarousel" class="carousel slide mx-md-5 rounded overflow-hidden mt-2" data-bs-ride="carousel">
     <div class="carousel-indicators">
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true"
+            aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
@@ -12,32 +11,20 @@
 
             <div class="container">
                 <div class="carousel-caption text-start">
-                    <h1>Example headline.</h1>
-                    <p>Some representative placeholder content for the first slide of the carousel.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                    <h1>Renkli, Canlı ve Kültür dolu.</h1>
+                    <p>Afrika modası koleksiyonumuzla renkli, canlı ve kültür dolu bir dünyaya adım atın.</p>
+                    <p><a class="btn btn btn-danger" href="#">Alışverişe başla</a></p>
                 </div>
             </div>
         </div>
         <div class="carousel-item">
-            
-            <img src="{{ asset('images/sli3.png') }}" class="img-fluid w-100 h-100 object-fit-cover " alt="slide1" />
-
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1>Another example headline.</h1>
-                    <p>Some representative placeholder content for the second slide of the carousel.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
-                </div>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="{{ asset('images/sli2.png') }}" class="img-fluid w-100 h-100 object-fit-cover " alt="slide1" />
+            <img src="{{ asset('images/sli4.png') }}" class="img-fluid w-100 h-100 object-fit-cover " alt="slide1" />
 
             <div class="container">
                 <div class="carousel-caption text-end">
-                    <h1>One more for good measure.</h1>
-                    <p>Some representative placeholder content for the third slide of this carousel.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                    <h1>Afrika modasının kalbi</h1>
+                    <p>Koleksiyonumuz, kıtanın en yetenekli tasarımcılarından gelen kıyafetleri içerir.</p>
+                    <p><a class="btn btn-danger" href="/signup">Hesap oluştur</a></p>
                 </div>
             </div>
         </div>
--- resources/views/partials/_search-bar.blade.php
+++ resources/views/partials/_search-bar.blade.php
@@ -1,9 +1,16 @@
-
-<form action="/products" class="row g-3 border rounded p-3">
-    <div class="col-auto flex-grow-1">
-        <input type="search" class="form-control text-secondary" id="searc-input" name="s">
-    </div>
-    <div class="col-auto">
-        <button type="submit" class="btn btn-primary px-3 px-md-5">Ara</button>
+<form action="/products" class="row justify-content-center py-3 px-md-5 text-bg-danger">
+    <div class="col col-md-8 bg-white d-flex rounded rounded-4 overflow-hidden px-0 mb-2">
+        <div class="col-auto flex-grow-1">
+            <input type="search" class="form-control text-secondary border-0 ps-4 rounded-0 rounded-left py-2" placeholder="ne aramıştınız?"
+                id="searc-input" name="s">
+        </div>
+        <button type="submit" class="btn btn-white rounded-0 text-danger">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-search" viewBox="0 0 16 16">
+                <path
+                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+            <span>Ara</span>
+        </button>
     </div>
 </form>
